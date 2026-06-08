5b45b4d0473dabecbbf821bcaa0cad5262465e5e
jerry <cow3530@163.com>
2026-06-08 14:36:24 +0800

    按需加载element-plus

--- frontend-vue3/src/App.vue
+++ frontend-vue3/src/App.vue
@@ -6,6 +6,19 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div class="button-demo">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.button-demo {
+  margin-top: 1rem;
+}
+</style>
